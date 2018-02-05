9cd7a51a25b2adc44169c9ee56fa33091aabdce8
teikk <k.lyzwa7@gmail.com>
2018-02-05 17:37:03 +0100

    create basic layout

---
+++ resources/views/_includes/navbar.blade.php
@@ -0,0 +1,20 @@
+<nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://bulma.io">
+      Drive
+    </a>
+
+    <button class="button navbar-burger">
+      <span></span>
+      <span></span>
+      <span></span>
+    </button>
+  </div>
+  <div class="navbar-menu">
+    <div class="navbar-start">
+      
+    </div>
+    <div class="navbar-end">
+
+    </div>
+</nav>
