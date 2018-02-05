4d418a204a408d91f7ba6f14301355e6f32c7c11
teikk <k.lyzwa7@gmail.com>
2018-02-05 19:14:14 +0100

    make user model use the new properties

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'nickname', 'email', 'password','first_name','last_name','is_driver'
     ];
 
     /**
