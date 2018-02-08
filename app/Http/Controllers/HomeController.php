22fe040badba22e13e5ced7065402f9f4935b230
teikk <k.lyzwa7@gmail.com>
2018-02-08 20:06:35 +0100

    make controller return different views depending if user is logged in

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,19 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     /**
      * Show the application dashboard.
      *
@@ -23,6 +14,15 @@ class HomeController extends Controller
      */
     public function index()
     {
+        if(Auth::check()) {
+          /**
+           * @todo Get users previous rides non rated rides 
+           * and suggest to rate them
+           */
+          $userRides = [];
+          return view('dashboard',['user_rides' => $userRides]);
+        }
+
         return view('home');
     }
 }
