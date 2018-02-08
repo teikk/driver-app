e8835f24ef838f86f9afc6050b35ad5a079b4c09
teikk <k.lyzwa7@gmail.com>
2018-02-08 20:06:52 +0100

    make separate layout for dashboard

---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('navbar.start')
+  <a href="#" class="navbar-item">Order a Car</a>
+  <a href="#" class="navbar-item">Previous rides</a>
+  <a href="#" class="navbar-item">Become a Driver</a>
+@endsection
