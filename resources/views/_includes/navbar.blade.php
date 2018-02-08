4248c92f4ab151191db0427c989a3c4f3313677a
teikk <k.lyzwa7@gmail.com>
2018-02-08 20:05:54 +0100

    add yield to navbar so views can add or remove elements

--- resources/views/_includes/navbar.blade.php
+++ resources/views/_includes/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
-    <a class="navbar-item" href="https://bulma.io">
+    <a class="navbar-item" href="{{route('home')}}">
       Drive
     </a>
 
@@ -12,9 +12,9 @@
   </div>
   <div class="navbar-menu">
     <div class="navbar-start">
-      
+      @yield('navbar.start')
     </div>
     <div class="navbar-end">
-
+      @yield('navbar.end')
     </div>
 </nav>
